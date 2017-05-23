e794e600c574f71e8b2c26fa27e6ae940cc84f1f
Tien Le <tiduslmt@gmail.com>
2017-05-23 14:45:27 +0800

    adding action creator

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { selectBook } from '../actions/index';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       );
     });
   }
@@ -25,4 +33,8 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
   return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
-    {title: 'Eloquent Ruby'}
+    {title: 'Javascript: The Good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 89},
+    {title: 'The Dark Tower', pages: 30},
+    {title: 'Eloquent Ruby', pages: 50}
   ];
 }
